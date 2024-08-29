0f6bc6d4590d2c7a012b348b96322173eb5296dd
Real-DJ-Lucky <75531507+Real-DJ-Lucky@users.noreply.github.com>
2024-08-29 16:13:31 -0500

    Add files via upload

---
+++ installPublic.lua
@@ -0,0 +1,7 @@
+shell.run("pastebin get jewpSesL startup")
+sleep(1)
+shell.run("wget run https://raw.githubusercontent.com/knijn/musicify/main/install.lua")
+sleep(1)
+shell.run("set musicify.repo https://raw.githubusercontent.com/Real-DJ-Lucky/musicify-songs/main/index.json")
+sleep(1)
+shell.run("reboot")
