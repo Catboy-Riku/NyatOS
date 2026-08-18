9971dd7b2f6e54721478828ad97bf678efdbb8b6
Riku <75531507+Catboy-Riku@users.noreply.github.com>
2026-08-18 13:46:09 -0500

    Update installSecure.lua

--- installSecure.lua
+++ installSecure.lua
@@ -1,9 +1,22 @@
 shell.run("pastebin get XFgHwpHA startup")
 sleep(1)
+
+shell.run("wget https://gist.githubusercontent.com/zyxkad/a5e02d16d244b20b026ecfb06f662210/raw/aes.lua aes.lua")
+sleep(1)
+
+shell.run("wget https://raw.githubusercontent.com/Egor-Skriptunoff/pure_lua_SHA/master/sha2.lua sha2.lua")
+sleep(1)
+
+shell.run("wget https://raw.githubusercontent.com/Catboy-Riku/NyatOS/main/nyatos_crypto.lua nyatos_crypto.lua")
+sleep(1)
+
 shell.run("wget https://raw.githubusercontent.com/simadude/buccshot/refs/heads/main/buccshot.lua buccshot.lua")
 sleep(1)
+
 shell.run("wget run https://raw.githubusercontent.com/knijn/musicify/main/install.lua")
 sleep(1)
+
 shell.run("set musicify.repo https://raw.githubusercontent.com/Catboy-Riku/NyatOS/main/index.json")
-sleep(5)
+sleep(3)
+
 shell.run("reboot")
